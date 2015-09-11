0b7c7be86fd701c4a98f63eb09c06f21565592d1
Reece Patterson <reece.patterson110@gmail.com>
2015-09-11 19:27:14 +1200

    Added jenny's twitch work. changed her API class to JsonHelper and simplified the method.

---
+++ FelisBotus/src/com/wc1/felisBotus/streamAPIs/JsonHelper.java
@@ -0,0 +1,35 @@
+package com.wc1.felisBotus.streamAPIs;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+/**
+ * Api class used to read Json from URL.
+ * @author JennyLeeP
+ *
+ */
+public class JsonHelper {
+
+	public static Gson gson = new Gson();
+
+	/**
+	 * Opens a buffered reader, reads the URL and closes the buffered reader.
+	 * @param urlString
+	 * @return
+	 * @throws Exception
+	 */
+	public static JsonObject readJsonFromUrl(String urlString) throws IOException {
+		InputStreamReader inStream = null;
+		try {
+			URL url = new URL(urlString);
+			inStream = new InputStreamReader(url.openStream());
+			return gson.fromJson(inStream, JsonObject.class);
+		} finally {
+			if (inStream != null)
+				inStream.close();
+		}
+	}
+}
---
+++ FelisBotus/src/com/wc1/felisBotus/streamAPIs/twitch/Twitch_Stream.java
@@ -0,0 +1,45 @@
+package com.wc1.felisBotus.streamAPIs.twitch;
+
+import com.google.gson.JsonObject;
+/**
+ * <pre>
+ * Class used to parse Json into objects.
+ * Objects are defined by their "tag" in the Json string.
+ *</pre>
+ * @author JennyLeeP
+ *
+ */
+public class Twitch_Stream {
+	boolean online;
+	String status;
+	String game;
+	String url;
+
+/**
+ * Method used to load JsonObjects from a Json String retrieved by the Twitch_Api.class.
+ * @param jObject
+ */
+	public void load(JsonObject jObject)
+	{
+		setStatus(jObject.get("status").getAsString());
+		setGame(jObject.get("game").getAsString());
+		setUrl(jObject.get("url").getAsString());
+	}
+
+	public boolean isOnline()
+	{
+		return this.online; }
+	public void setOnline(boolean online) { this.online = online; }
+
+	public String getStatus() {
+		return this.status; }
+	public void setStatus(String status) { this.status = status; }
+	
+	public String getGame() {
+		return this.game; }
+	public void setGame(String game) { this.game = game; }
+	
+	public String getUrl() {
+		return this.url; }
+	public void setUrl(String url) { this.url = url; }
+}
